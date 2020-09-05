4074dc3584ec8320b30a804976ff3deba1693e47
Nirbhay <da201501068@gmail.com>
2020-09-05 22:53:19 +0530

    added openamweather api call

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import axios, { AxiosRequestConfig } from 'axios';
 
 export default function App() {
+
+
+  let config: AxiosRequestConfig = {
+    method: 'get',
+    url: 'https://api.openweathermap.org/data/2.5/onecall?lat=20.79&lon=70.7&exclude=minutely&appid=c1e64b484782aada5e07d493b0c358fb',
+    headers: {}
+  };
+
+  axios(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
