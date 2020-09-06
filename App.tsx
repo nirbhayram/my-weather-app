03a1b95c4c955123cb76e5819ab44aaf3986df72
Nirbhay <da201501068@gmail.com>
2020-09-06 12:22:23 +0530

    added input box to enter city name

--- App.tsx
+++ App.tsx
@@ -1,31 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import axios, { AxiosRequestConfig } from 'axios';
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Input,
+  Item,
+  Button,
+  Text,
+} from "native-base";
+import axios, { AxiosRequestConfig } from "axios";
 
 export default function App() {
+  const [place, setPlace] = useState("pune");
 
-
-  let config: AxiosRequestConfig = {
-    method: 'get',
-    url: 'https://api.openweathermap.org/data/2.5/onecall?lat=20.79&lon=70.7&exclude=minutely&appid=c1e64b484782aada5e07d493b0c358fb',
-    headers: {}
+  const call = (cityName:string) => {
+    let config: AxiosRequestConfig = {
+      method: "get",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=c1e64b484782aada5e07d493b0c358fb&units=metric`,
+      headers: {},
+    };
+    console.log(`getting value for ${cityName}`)
+    axios(config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  axios(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Header />
+      <Content>
+        <Item regular>
+          <Input
+            placeholder="Regular Textbox"
+            onChangeText={(text) => {
+              setPlace(text);
+            }}
+          />
+        </Item>
+        <Button block success onPress={()=>{
+          call(place)
+        }}>
+          <Text>Success</Text>
+        </Button>
+      </Content>
+    </Container>
   );
 }
 
