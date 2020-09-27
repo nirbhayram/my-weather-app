a78b383fdfb9edf5790f256729c4bebd61eb2d70
Nirbhay <da201501068@gmail.com>
2020-09-27 19:54:39 +0530

    linked hero section

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -1,19 +1,37 @@
+import { observer } from 'mobx-react';
+import { Spinner } from 'native-base';
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import store from '../../store/mobx/CityStore';
 import TinyInfoDisplay from './TinyInfoDisplay';
 
-const HeroSection = () => {
+const HeroSection = observer(() => {
+    function getTime(date:Date): string{
+        return `${date.getHours()}:${date.getMinutes()}`
+    }
+
     return (
         <View style={styles.container}>
-            <TinyInfoDisplay icon='sun' text="6:15 am" />
-            <TinyInfoDisplay icon="umbrella-beach" text="5:26 pm" />
-            <TinyInfoDisplay icon='wind' text="17 km/hr" />
-            <TinyInfoDisplay icon='cloud-sun-rain' text="11%" />
-            <TinyInfoDisplay icon='bolt' text="UV: 3" />
-            <TinyInfoDisplay icon='tint' text="17%" />
+            {
+                store.isEmpty ? (
+                    <>
+                        <Spinner color='white'/>
+                    </>
+                ) :
+                    (
+                        <>
+                            <TinyInfoDisplay icon='sun' text={getTime(store.listCity[0].dailyData[0].sunrise)} load='false' />
+                            <TinyInfoDisplay icon="umbrella-beach" text={getTime(store.listCity[0].dailyData[0].sunset)} load='false' />
+                            <TinyInfoDisplay icon='wind' text={`${store.listCity[0].dailyData[0].wind_speed} m/s`} load='false' />
+                            <TinyInfoDisplay icon='cloud-sun-rain' text={`${store.listCity[0].dailyData[0].probablity_precipitation*100} %`} load='false' />
+                            <TinyInfoDisplay icon='bolt' text={`UV: ${store.listCity[0].dailyData[0].uvi}`} load='false' />
+                            <TinyInfoDisplay icon='tint' text={`${store.listCity[0].dailyData[0].dew_drops}`} load='false' />
+                        </>
+                    )
+            }
         </View>
     )
-}
+})
 
 export default HeroSection
 
--- components/HeroSection/TinyInfoDisplay.tsx
+++ components/HeroSection/TinyInfoDisplay.tsx
@@ -1,15 +1,16 @@
+
 import React from 'react'
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import { Icon } from 'react-native-elements'
 
-const TinyInfoDisplay = ({icon="sun-o",text="6:15 am"}) => {
+const TinyInfoDisplay = ({ icon = "sun-o", text = "6:15 am" }) => {
     return (
         <View style={styles.labelView}>
             <Icon
                 name={icon}
                 type='font-awesome-5'
                 color='#EBEBEB'
-                size={Dimensions.get('window').width*0.06} />
+                size={Dimensions.get('window').width * 0.06} />
             <View style={styles.labelViewTextView}>
                 <Text style={styles.labelViewTextViewText}>{text}</Text>
             </View>
@@ -21,7 +22,7 @@ export default TinyInfoDisplay
 
 const styles = StyleSheet.create({
     labelView: {
-        minWidth: Dimensions.get('window').width*0.20,
+        minWidth: Dimensions.get('window').width * 0.20,
         flexDirection: "column",
         flex: 1,
         paddingTop: 10,
@@ -35,7 +36,7 @@ const styles = StyleSheet.create({
     },
     labelViewTextViewText: {
         color: "#EBEBEB",
-        fontSize: Dimensions.get('window').width*0.04,
+        fontSize: Dimensions.get('window').width * 0.04,
         padding: "10%"
     }
 })
