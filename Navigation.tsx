560d98f9a555b12ab7d3d4af3bfa03251c7d394c
Nirbhay <da201501068@gmail.com>
2020-09-30 21:58:39 +0530

    new store configured for navigation screen

--- Navigation.tsx
+++ Navigation.tsx
@@ -10,7 +10,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { Dialog } from 'react-native-simple-dialogs';
 import { City } from './components/pojo/City';
 import { getCityAtmosphereDetails } from './components/utils/RestUtils';
-import store from './store/mobx/CityStore';
+import store, {CityStoreObject} from './store/mobx/NewCityStore';
 import Toast from "react-native-root-toast";
 
 const DATA = [
@@ -44,8 +44,8 @@ const Navigation = observer(({ navigation }) => {
     const Item = (data: Object) => (
 
         <TouchableOpacity onPress={() => {
-            store.changeCity(data.indexCity);
-            goToMainScreen()
+            // store.changeCity(data.indexCity);
+            // goToMainScreen()
         }}>
             <View style={styles.flatListView}>
                 <LinearGradient colors={["#4064e0", "#b6c5fb"]} style={styles.flatListItemView}>
@@ -63,91 +63,6 @@ const Navigation = observer(({ navigation }) => {
 
         const [city, setCity] = useState('')
 
-        const getCityDetails = async (cityName: string) => {
-            await getCityAtmosphereDetails(cityName, onSuccess, onError);
-        };
-
-        const onSuccess = (city: City) => {
-            store.setCity(city);
-            console.log('city name' + city.name);
-            console.log('city lat ' + city.lat);
-            console.log('city lon ' + city.lon);
-            console.log('city weather id ' + city.weather.id);
-            console.log('city weather main ' + city.weather.main);
-            console.log('city weather description ' + city.weather.description);
-            console.log('city weather icon ' + city.weather.icon);
-            console.log('city temperature ' + city.temperature);
-            console.log('city temprature min ' + city.temperature_min);
-            console.log('city temprature max ' + city.temperature_max);
-            console.log('city presure ' + city.pressure);
-            console.log('city humidity' + city.humidity);
-            console.log('city sea level' + city.sea_level);
-            console.log('city ground level' + city.grnd_level);
-            console.log('city visibility ' + city.visibility);
-            console.log('city wind speed ' + city.wind_speed);
-            console.log('city wind degree ' + city.wind_degree);
-            console.log('city hourly data time ' + city.hourlyData[0].time.toString());
-            console.log('city hourly data temperature ' + city.hourlyData[0].temperature);
-            console.log('city hourly data pressure ' + city.hourlyData[0].pressure);
-            console.log('city hourly data humidity ' + city.hourlyData[0].humidity);
-            console.log('city hourly data clouds ' + city.hourlyData[0].clouds);
-            console.log('city hourly data visibility ' + city.hourlyData[0].visibility);
-            console.log('city hourly data wind speed ' + city.hourlyData[0].wind_speed);
-            console.log('city hourly data wind degree ' + city.hourlyData[0].wind_deg);
-            console.log('city hourly data weather id ' + city.hourlyData[0].weather.id);
-            console.log('city hourly data weather main ' + city.hourlyData[0].weather.main);
-            console.log('city hourly data weather description ' + city.hourlyData[0].weather.description);
-            console.log('city hourly data weather icon ' + city.hourlyData[0].weather.icon);
-            console.log('city hourly data probability precepitation ' + city.hourlyData[0].probablity_precipitation);
-            console.log('city hourly data rain ' + city.hourlyData[4].rain);
-            console.log('city daily data date' + city.dailyData[0].date.toString());
-            console.log('city daily data sunrise' + city.dailyData[0].sunrise.toString());
-            console.log('city daily data sun set ' + city.dailyData[0].sunset.toString());
-            console.log('city daily data temperature day ' + city.dailyData[0].temperature_day);
-            console.log('city daily data temperature min ' + city.dailyData[0].temperature_min);
-            console.log('city daily data temperature max ' + city.dailyData[0].temperature_max);
-            console.log('city daily data temperature evening ' + city.dailyData[0].temperature_evening);
-            console.log('city daily data temperature night ' + city.dailyData[0].temperature_night);
-            console.log('city daily data temperarure morning ' + city.dailyData[0].temperature_morning);
-            console.log('city daily data pressure ' + city.dailyData[0].pressure);
-            console.log('city daily data humidity ' + city.dailyData[0].humidity);
-            console.log('city daily data dew drops ' + city.dailyData[0].dew_drops);
-            console.log('city daily data wind speed ' + city.dailyData[0].wind_speed);
-            console.log('city daily data wind degree ' + city.dailyData[0].wind_deg);
-            console.log('city daily data weather id ' + city.dailyData[0].weather.id);
-            console.log('city daily data weather main ' + city.dailyData[0].weather.main);
-            console.log('city daily data weather description ' + city.dailyData[0].weather.description);
-            console.log('city daily data weather icon ' + city.dailyData[0].weather.icon);
-            console.log('city daily data clouds ' + city.dailyData[0].clouds);
-            console.log('city daily data probability of precipitation ' + city.dailyData[0].probablity_precipitation);
-            console.log('city daily data rain ' + city.dailyData[0].rain);
-            console.log('city daily data uvi ' + city.dailyData[0].uvi);
-            setLoading(false)
-            setDialogVisible(false)
-        };
-
-        const onError = (error: AxiosError) => {
-            console.log(error);
-            setLoading(false);
-            setDialogVisible(false)
-            Toast.show('Are you sure you spell city correctly?', {
-                duration: Toast.durations.SHORT,
-                position: Toast.positions.CENTER,
-                shadow: true,
-                animation: true,
-                hideOnPress: true,
-                delay: 0,
-                onShow: () => {
-                },
-                onShown: () => {
-                },
-                onHide: () => {
-                },
-                onHidden: () => {
-                }
-            });
-        };
-
         return (
             <View>
                 {
@@ -162,7 +77,29 @@ const Navigation = observer(({ navigation }) => {
                                 </Input>
                                 <Button block style={{ backgroundColor: "#b6c5fb" }} onPress={() => {
                                     setLoading(true)
-                                    getCityDetails(city);
+                                    store.setCity(city, () => {
+                                        setLoading(false);
+                                        setDialogVisible(false);
+                                    }, () => {
+                                        setLoading(false);
+                                        setDialogVisible(false)
+                                        Toast.show('Are you sure you spell city correctly?', {
+                                            duration: Toast.durations.SHORT,
+                                            position: Toast.positions.CENTER,
+                                            shadow: true,
+                                            animation: true,
+                                            hideOnPress: true,
+                                            delay: 0,
+                                            onShow: () => {
+                                            },
+                                            onShown: () => {
+                                            },
+                                            onHide: () => {
+                                            },
+                                            onHidden: () => {
+                                            }
+                                        });
+                                    })
                                 }}>
                                     <Text style={{ fontSize: 15, color: 'white', fontWeight: 'bold' }}>Add city</Text>
                                 </Button>
@@ -175,16 +112,16 @@ const Navigation = observer(({ navigation }) => {
     }
 
     const renderItem = ({ item, index }) => (
-        <Item cityName={item.name} icon={item.weather.icon} indexCity={index} />
+        <Item cityName={item.city.name} icon={item.city.weather.icon} indexCity={index} />
     );
 
     return (
         <SafeAreaView style={[styles.container]}>
             <FlatList
                 style={[{ width: Dimensions.get('screen').width }, styles.flatList]}
-                data={store.listCity.slice()}
+                data={Array.from(store.cities.values())}
                 renderItem={renderItem}
-                keyExtractor={item => item.name}
+                keyExtractor={(item:CityStoreObject) => item.cityName}
             />
             <View style={styles.inputView}>
                 <Button block style={{ backgroundColor: "#b6c5fb" }} onPress={() => {
---
+++ store/mobx/NewCityStore.ts
@@ -0,0 +1,78 @@
+import { AxiosError } from "axios";
+import { action, observable } from "mobx";
+import useRestCall from "../../components/hooks/useRestCall";
+import { City } from "../../components/pojo/City";
+
+export class CityStoreObject {
+
+    @observable city: City | undefined;
+    @observable cityName: string;
+    @observable expiry: number | undefined;
+    @observable isLoading: boolean;
+
+    EXPIRY_IN_MINUTE: number = 1;
+
+    constructor(cityName:string) {
+        this.cityName = cityName;
+        this.isLoading = true;
+    }
+
+    @action setCity(city: City) {
+        this.city = city;
+        this.expiry = Date.now() + this.EXPIRY_IN_MINUTE * 1000;
+        this.loadingComplete();
+    }
+
+    public get getCity(): City | undefined {
+        return this.city;
+    }
+
+    @action loadingComplete() {
+        this.isLoading = false;
+    }
+
+    @action loadingStarted() {
+        this.isLoading = true;
+    }
+}
+
+class NewCityStore {
+
+    @observable cities: Map<string, CityStoreObject>;
+
+    constructor() {
+        this.cities = new Map<string, CityStoreObject>();
+    }
+
+    @action setCity(cityName: string, onSuccessExecution: Function, onFailedExecution: Function) {
+        cityName = cityName.toLowerCase()
+        if (!this.cities.has(cityName)) {
+            useRestCall(cityName, (city: City) => {
+                const cityStoreObject: CityStoreObject = new CityStoreObject(cityName);
+                cityStoreObject.setCity(city);
+                this.cities.set(cityName, cityStoreObject);
+                onSuccessExecution();
+            }, onFailedExecution);
+        }else{
+            onSuccessExecution();
+        }
+    }
+
+    public getCity(cityName: string): CityStoreObject | undefined {
+        cityName = cityName.toLowerCase()
+        const cityStoreObject = this.cities.get(cityName);
+
+        if (typeof (cityStoreObject) !== undefined && Date.now() > cityStoreObject.expiry) {
+            cityStoreObject.loadingStarted()
+            useRestCall(cityName, (city: City) => {
+                cityStoreObject.setCity(city);
+            }, (error: AxiosError) => { console.log(error) });
+        }
+        return cityStoreObject;
+    }
+
+}
+
+const store:NewCityStore = new NewCityStore();
+
+export default store;
