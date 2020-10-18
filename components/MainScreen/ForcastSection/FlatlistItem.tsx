89b160ae8c1854f69ae02584792a14cf9c08b5ae
Nirbhay <da201501068@gmail.com>
2020-10-18 15:57:54 +0530

    added eslint and prettier

--- components/MainScreen/ForcastSection/FlatlistItem.tsx
+++ components/MainScreen/ForcastSection/FlatlistItem.tsx
@@ -1,36 +1,24 @@
-import {observer} from 'mobx-react'
-import React from 'react'
-import {Image, StyleSheet, Text, View} from 'react-native'
-
-const FlatlistItem = observer((data: { time: string, icon: string, text: number }) => (
-    <View style={[styles.component]}>
-        <Text style={[styles.componentTitleText]}>{data.time}</Text>
-        <Image
-            style={styles.imageView}
-            source={{ uri: `http://openweathermap.org/img/wn/${data.icon}@2x.png` }}
-        />
-        <Text style={[styles.componentBottomText]}>{data.text}°</Text>
-    </View>
-))
-
-export default FlatlistItem
+import React from 'react';
+import {
+    Image, StyleSheet, Text, View,
+} from 'react-native';
 
 const styles = StyleSheet.create({
     component: {
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
         height: 120,
         paddingRight: 20,
         paddingLeft: 20,
         borderRadius: 15,
         borderWidth: 1,
-        borderColor: "#EBEBEB",
+        borderColor: '#EBEBEB',
         marginLeft: 10,
-        marginRight: 10
+        marginRight: 10,
     },
     componentTitleText: {
-        color: "#fff",
+        color: '#fff',
         fontSize: 15,
         paddingBottom: 5,
     },
@@ -38,12 +26,30 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
     },
     componentBottomText: {
-        color: "#fff",
+        color: '#fff',
         fontSize: 15,
         paddingBottom: 5,
     },
     imageView: {
         width: 50,
         height: 50,
-    }
-})
+    },
+});
+
+const FlatlistItem = (data: { time: string; icon: string; text: number }) => (
+    <View style={[styles.component]}>
+        <Text style={[styles.componentTitleText]}>{data.time}</Text>
+        <Image
+            style={styles.imageView}
+            source={{
+                uri: `http://openweathermap.org/img/wn/${data.icon}@2x.png`,
+            }}
+        />
+        <Text style={[styles.componentBottomText]}>
+{data.text}
+°
+        </Text>
+    </View>
+);
+
+export default FlatlistItem;
--- components/MainScreen/ForcastSection/index.tsx
+++ components/MainScreen/ForcastSection/index.tsx
@@ -1,36 +1,38 @@
-import {Spinner} from 'native-base';
-import React from 'react'
-import {FlatList} from 'react-native'
-import {getTime} from '../../../utils/utilities';
+import { Spinner } from 'native-base';
+import React from 'react';
+import { FlatList } from 'react-native';
+import { getTime } from '../../../utils/utilities';
 import FlatlistItem from './FlatlistItem';
-import {City} from "../../../utils/typeDef";
-import useGetCity from "../../../hooks/useGetCity";
+import { City } from '../../../utils/typeDef';
+import useGetCity from '../../../hooks/useGetCity';
 
 const ForcastSection = () => {
-
     const city: City | undefined = useGetCity();
 
-    const renderItem = (data: { item: { time: number, icon: string, temperature: number } }) => (
-        <FlatlistItem time={getTime(new Date(data.item.time * 1000))} icon={data.item.icon}
-                      text={data.item.temperature}/>
+    const renderItem = (data: {
+        item: { time: number; icon: string; temperature: number };
+    }) => (
+        <FlatlistItem
+            time={getTime(new Date(data.item.time * 1000))}
+            icon={data.item.icon}
+            text={data.item.temperature}
+        />
     );
 
     return (
         <>
-            {
-                city ? (
-                    <FlatList
-                        horizontal={true}
-                        data={city.hourData}
-                        renderItem={renderItem}
-                        keyExtractor={item => `${item.time}`}
-                    />
-                ) : (
-                    <Spinner color='white'/>
-                )
-            }
+            {city ? (
+                <FlatList
+                    horizontal
+                    data={city.hourData}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => `${item.time}`}
+                />
+            ) : (
+                <Spinner color="white" />
+            )}
         </>
-    )
-}
+    );
+};
 
-export default ForcastSection
+export default ForcastSection;
