fed452cf750eab28b173b3280b079d80f1804bbd
Nirbhay <da201501068@gmail.com>
2020-10-18 15:51:58 +0530

    added eslint and prettier

--- components/bottomSheet/FlatList.tsx
+++ components/bottomSheet/FlatList.tsx
@@ -1,52 +1,55 @@
-import {Spinner} from 'native-base';
+import { Spinner } from 'native-base';
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
-import {getDate} from '../../utils/utilities';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { getDate } from '../../utils/utilities';
 import FlatlistItem from './FlatlistItem';
-import useGetCity from "../../hooks/useGetCity";
-import {ICON_COLOR} from "../../utils/stylesConstants";
-
-const BottomSectionFlatList = () => {
-
-    const city = useGetCity();
-
-    const renderItem = (data: { item: { date: number, icon: string, minTemperature: number, maxTemperature: number } }) => (
-        <FlatlistItem
-            icon={data.item.icon}
-            date={getDate(new Date(data.item.date * 1000))}
-            maxTemperature={data.item.maxTemperature}
-            minTemperature={data.item.minTemperature}/>
-    );
-
-    return (
-        <View style={styles.container}>
-            {
-                city ? (
-                        <FlatList
-                            data={city.dailyData}
-                            renderItem={renderItem}
-                            keyExtractor={item => `${item.date}`}
-                        />
-                    )
-                    :
-                    (
-                        <Spinner color={ICON_COLOR}/>
-                    )
-            }
-        </View>
-    );
-};
-
-export default BottomSectionFlatList;
+import useGetCity from '../../hooks/useGetCity';
+import { ICON_COLOR } from '../../utils/stylesConstants';
 
 const styles = StyleSheet.create({
     container: {
         flexBasis: 'auto',
         flexShrink: 1,
         justifyContent: 'center',
-        flexDirection: "column",
+        flexDirection: 'column',
         backgroundColor: 'rgba(255,255,255,0.6)',
         borderRadius: 15,
-        marginBottom: 10
+        marginBottom: 10,
     },
 });
+
+const BottomSectionFlatList = () => {
+    const city = useGetCity();
+
+    const renderItem = (data: {
+        item: {
+            date: number;
+            icon: string;
+            minTemperature: number;
+            maxTemperature: number;
+        };
+    }) => (
+        <FlatlistItem
+            icon={data.item.icon}
+            date={getDate(new Date(data.item.date * 1000))}
+            maxTemperature={data.item.maxTemperature}
+            minTemperature={data.item.minTemperature}
+        />
+    );
+
+    return (
+        <View style={styles.container}>
+            {city ? (
+                <FlatList
+                    data={city.dailyData}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => `${item.date}`}
+                />
+            ) : (
+                <Spinner color={ICON_COLOR} />
+            )}
+        </View>
+    );
+};
+
+export default BottomSectionFlatList;
--- components/bottomSheet/index.tsx
+++ components/bottomSheet/index.tsx
@@ -1,30 +1,24 @@
-import React from 'react'
-import {StyleSheet, View} from 'react-native'
-import BottomSectionFlatList from './FlatList'
-import BottomSectionHeroSection from './HeroSection'
-import BottomSectionTitle from './Title'
+import React, { ReactElement } from 'react';
+import { StyleSheet, View } from 'react-native';
+import BottomSectionFlatList from './FlatList';
+import BottomSectionHeroSection from './HeroSection';
+import BottomSectionTitle from './Title';
 
-const BottomSection = () => {
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        backgroundColor: '#f0f3f5',
+        paddingHorizontal: 20,
+        height: '100%',
+    },
+});
+const BottomSection: React.FC = (): ReactElement => (
+    <View style={[styles.container]}>
+        <BottomSectionTitle />
+        <BottomSectionHeroSection />
+        <BottomSectionFlatList />
+    </View>
+);
 
-    return (
-        <View style={[styles.container]}>
-            <BottomSectionTitle/>
-            <BottomSectionHeroSection/>
-            <BottomSectionFlatList/>
-        </View>
-    )
-}
-
-export default BottomSection
-
-const styles = StyleSheet.create(
-    {
-        container: {
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            backgroundColor: "#f0f3f5",
-            paddingHorizontal: 20,
-            height: '100%'
-        }
-    }
-)
+export default BottomSection;
