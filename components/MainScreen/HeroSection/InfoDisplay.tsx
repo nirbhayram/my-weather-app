9d368da99048e4ed9fb3a80e7d615785a949a3c2
Nirbhay <da201501068@gmail.com>
2020-10-18 15:59:31 +0530

    added eslint and prettier

--- components/MainScreen/HeroSection/InfoDisplay.tsx
+++ components/MainScreen/HeroSection/InfoDisplay.tsx
@@ -1,46 +1,47 @@
-import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import {Icon} from 'react-native-elements'
-import {useDimensions} from "@react-native-community/hooks";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { useDimensions } from '@react-native-community/hooks';
 
-const InfoDisplay = ({icon = "sun-o", text = "6:15 am"}) => {
-    const {width} = useDimensions().window
+const styles = StyleSheet.create({
+    labelView: {
+        flexDirection: 'column',
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    labelViewTextView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    labelViewTextViewText: {
+        color: '#EBEBEB',
+        fontSize: 15,
+        padding: 10,
+    },
+    infoDisplay: {
+        flex: 1,
+        flexBasis: 'auto',
+        flexGrow: 1,
+    },
+});
+
+const InfoDisplay = ({ icon = 'sun-o', text = '6:15 am' }) => {
+    const { width } = useDimensions().window;
     return (
-        <View style={[styles.infoDisplay, {minWidth: width / 4}]}>
+        <View style={[styles.infoDisplay, { minWidth: width / 4 }]}>
             <View style={styles.labelView}>
                 <Icon
                     name={icon}
-                    type='font-awesome-5'
-                    color='#EBEBEB'
-                    size={25}/>
+                    type="font-awesome-5"
+                    color="#EBEBEB"
+                    size={25}
+                />
                 <View style={styles.labelViewTextView}>
                     <Text style={styles.labelViewTextViewText}>{text}</Text>
                 </View>
             </View>
         </View>
-    )
-}
+    );
+};
 
-export default InfoDisplay
-
-const styles = StyleSheet.create({
-    labelView: {
-        flexDirection: "column",
-        paddingTop: 10,
-        paddingBottom: 10,
-    },
-    labelViewTextView: {
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    labelViewTextViewText: {
-        color: "#EBEBEB",
-        fontSize: 15,
-        padding: 10
-    },
-    infoDisplay: {
-        flex: 1,
-        flexBasis: 'auto',
-        flexGrow: 1
-    }
-})
+export default InfoDisplay;
