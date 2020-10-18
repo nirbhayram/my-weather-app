cb5ebb91ea397bd4aad7a13f2ef33dc43793acdf
Nirbhay <da201501068@gmail.com>
2020-10-18 15:58:28 +0530

    added eslint and prettier

--- components/MainScreen/HeaderSection/index.tsx
+++ components/MainScreen/HeaderSection/index.tsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import {GestureResponderEvent, StyleSheet, View} from 'react-native'
-import {Icon} from "react-native-elements";
-import {useDimensions} from "@react-native-community/hooks";
-
-const HeaderSection = (prop: { navigateToNavigationScreen: (event: GestureResponderEvent) => void }) => {
-    const {width} = useDimensions().window;
-    const {navigateToNavigationScreen} = prop;
-    return (
-        <View style={[styles.navigationControl, {width: width}]}>
-            <Icon
-                name='navicon'
-                type='font-awesome'
-                color='#fff'
-                onPress={navigateToNavigationScreen}
-                size={25}/>
-            <Icon
-                name='bell-o'
-                type='font-awesome'
-                color='#fff'
-                size={25}/>
-        </View>
-    )
-}
-
-export default HeaderSection
+import React from 'react';
+import { GestureResponderEvent, StyleSheet, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { useDimensions } from '@react-native-community/hooks';
 
 const styles = StyleSheet.create({
     navigationControl: {
         flex: 0.3,
         flexBasis: 'auto',
         padding: 20,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
     },
-})
+});
+
+const HeaderSection = (prop: {
+    navigateToNavigationScreen: (event: GestureResponderEvent) => void;
+}) => {
+    const { width } = useDimensions().window;
+    const { navigateToNavigationScreen } = prop;
+    return (
+        <View style={[styles.navigationControl, { width }]}>
+            <Icon
+                name="navicon"
+                type="font-awesome"
+                color="#fff"
+                onPress={navigateToNavigationScreen}
+                size={25}
+            />
+            <Icon name="bell-o" type="font-awesome" color="#fff" size={25} />
+        </View>
+    );
+};
+
+export default HeaderSection;
