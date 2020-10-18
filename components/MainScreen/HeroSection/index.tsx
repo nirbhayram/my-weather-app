8607b44dd6d6b2dbe37a48698cf6ffc2ebdbb2b5
Nirbhay <da201501068@gmail.com>
2020-10-18 15:58:56 +0530

    added eslint and prettier

--- components/MainScreen/HeroSection/index.tsx
+++ components/MainScreen/HeroSection/index.tsx
@@ -1,77 +1,76 @@
-import {Spinner} from 'native-base';
-import React from 'react'
-import {StyleSheet, View} from 'react-native'
-import {getFixedDigitNumber, getTime} from '../../../utils/utilities';
+import { Spinner } from 'native-base';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { getFixedDigitNumber, getTime } from '../../../utils/utilities';
 import InfoDisplay from './InfoDisplay';
-import {City} from "../../../utils/typeDef";
-import useGetCity from "../../../hooks/useGetCity";
-
-const HeroSection = () => {
-    const city: City | undefined = useGetCity();
-    return (
-        <View style={[styles.container,]}>
-            {
-                city ? (
-                        [
-                            {
-                                icon: 'sun',
-                                text: getTime(new Date(city.current.sunrise * 1000))
-                            },
-                            {
-                                icon: 'umbrella-beach',
-                                text: getTime(new Date(city.current.sunset * 1000))
-                            },
-                            {
-                                icon: 'wind',
-                                text: getFixedDigitNumber(city.current.windSpeed)
-                            },
-                            {
-                                icon: 'cloud-sun-rain',
-                                text: getFixedDigitNumber(city.current.pop * 100)
-                            },
-                            {
-                                icon: 'bolt',
-                                text: getFixedDigitNumber(city.current.uv)
-                            },
-                            {
-                                icon: 'tint',
-                                text: getFixedDigitNumber(city.current.dewDrops)
-                            },
-                        ].map(
-                            (item) => (
-                                <InfoDisplay key={item.icon} icon={item.icon} text={item.text}/>
-                            )
-                        )
-                    ) :
-                    (
-                        <Spinner color='white'/>
-                    )
-            }
-        </View>
-    )
-}
-
-export default HeroSection
+import { City } from '../../../utils/typeDef';
+import useGetCity from '../../../hooks/useGetCity';
 
 const styles = StyleSheet.create({
     container: {
         flex: 3,
-        flexDirection: "row",
+        flexDirection: 'row',
         padding: 10,
         marginHorizontal: 30,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
         alignSelf: 'stretch',
         borderRadius: 20,
         backgroundColor: 'rgba(99, 128, 226,0.6)',
-        flexWrap: "wrap",
-        alignContent: "center",
+        flexWrap: 'wrap',
+        alignContent: 'center',
         flexGrow: 1,
         flexBasis: 'auto',
     },
     infoDisplay: {
         flex: 1,
         flexBasis: 'auto',
-        flexGrow: 1
-    }
-})
+        flexGrow: 1,
+    },
+});
+
+const HeroSection = () => {
+    const city: City | undefined = useGetCity();
+    return (
+        <View style={[styles.container]}>
+            {city ? (
+                [
+                    {
+                        icon: 'sun',
+                        text: getTime(new Date(city.current.sunrise * 1000)),
+                    },
+                    {
+                        icon: 'umbrella-beach',
+                        text: getTime(new Date(city.current.sunset * 1000)),
+                    },
+                    {
+                        icon: 'wind',
+                        text: getFixedDigitNumber(city.current.windSpeed),
+                    },
+                    {
+                        icon: 'cloud-sun-rain',
+                        text: getFixedDigitNumber(city.current.pop * 100),
+                    },
+                    {
+                        icon: 'bolt',
+                        text: getFixedDigitNumber(city.current.uv),
+                    },
+                    {
+                        icon: 'tint',
+                        text: getFixedDigitNumber(city.current.dewDrops),
+                    },
+                ].map((item) => (
+                    <InfoDisplay
+                        key={item.icon}
+                        icon={item.icon}
+                        text={item.text}
+                    />
+                ))
+            ) : (
+                <Spinner color="white" />
+            )}
+        </View>
+    );
+};
+
+export default HeroSection;
