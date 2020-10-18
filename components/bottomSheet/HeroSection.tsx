9e56d8120f5a58e1ad74cb69f994b2da4876c5f5
Nirbhay <da201501068@gmail.com>
2020-10-18 15:52:12 +0530

    added eslint and prettier

--- components/bottomSheet/HeroSection.tsx
+++ components/bottomSheet/HeroSection.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Spinner} from 'native-base';
-import {StyleSheet, View} from 'react-native';
-import {getFixedDigitNumber} from '../../utils/utilities';
+import { Spinner } from 'native-base';
+import { StyleSheet, View } from 'react-native';
+import { getFixedDigitNumber } from '../../utils/utilities';
 import HeroSectionLabel from './HeroSectionLabel';
 import HeroTitleSection from './HeroTitleSection';
 import useGetCity from '../../hooks/useGetCity';
-import {ICON_COLOR} from '../../utils/stylesConstants';
+import { ICON_COLOR } from '../../utils/stylesConstants';
 
 const styles = StyleSheet.create({
     container: {
@@ -30,34 +30,46 @@ const BottomSectionHeroSection = () => {
     const city = useGetCity();
     return (
         <View style={styles.container}>
-            {
-                city ? (
-                    <>
-                        <HeroTitleSection city={city}/>
-                        <View style={styles.horizontalRuler}/>
-                        <View style={styles.contentContainer}>
+            {city ? (
+                <>
+                    <HeroTitleSection city={city} />
+                    <View style={styles.horizontalRuler} />
+                    <View style={styles.contentContainer}>
+                        {[
                             {
-                                [
-                                    {keyText: 'Humidity', valueText: `${getFixedDigitNumber(city.current.humidity)} %`},
-                                    {keyText: 'Wind', valueText: `${getFixedDigitNumber(city.current.windSpeed)} m/s`},
-                                    {keyText: 'UV Index', valueText: getFixedDigitNumber(city.current.uv)},
-                                    {keyText: 'Rain', valueText: `${getFixedDigitNumber(city.current.pop * 100)} %`},
-                                ]
-                                    .map((item) => (
-                                        <HeroSectionLabel
-                                            key={item.keyText}
-                                            keyText={item.keyText}
-                                            valueText={item.valueText}
-                                        />
-                                    ))
-                            }
-                        </View>
-                    </>
-                ) : (
-                    <Spinner color={ICON_COLOR}/>
-                )
-            }
-
+                                keyText: 'Humidity',
+                                valueText: `${getFixedDigitNumber(
+                                    city.current.humidity,
+                                )} %`,
+                            },
+                            {
+                                keyText: 'Wind',
+                                valueText: `${getFixedDigitNumber(
+                                    city.current.windSpeed,
+                                )} m/s`,
+                            },
+                            {
+                                keyText: 'UV Index',
+                                valueText: getFixedDigitNumber(city.current.uv),
+                            },
+                            {
+                                keyText: 'Rain',
+                                valueText: `${getFixedDigitNumber(
+                                    city.current.pop * 100,
+                                )} %`,
+                            },
+                        ].map((item) => (
+                            <HeroSectionLabel
+                                key={item.keyText}
+                                keyText={item.keyText}
+                                valueText={item.valueText}
+                            />
+                        ))}
+                    </View>
+                </>
+            ) : (
+                <Spinner color={ICON_COLOR} />
+            )}
         </View>
     );
 };
